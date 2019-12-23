6f4a4f5f8ddae6e9f1722124d40f5c85e6593325
KevineMusoni <kevinenicky@gmail.com>
2019-12-23 09:41:54 -1200

    mail

--- email.php
+++ email.php
@@ -1,19 +1,22 @@
 <?php
-if(isset($_POST['submit'])){
-    $name=$_POST['name'];
-    $email=$_POST['email'];
-    $msg=$_POST['msg'];
-
-    $to='kevinenicky@gmail.com';
-    $subject='Form Submission';
-    $message="Name:".$name."\n"."Wrote the following: "."\n\n".$msg;
-    $headers="From:".$email;
-
-    if(mail($to, $subject, $message, $headers)){
-        echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-    }
-    else{
-        echo "something went wrong!"
-    }
+if(!isset($_POST['submit'])){
+    echo"error; you need to submit the form..."
 }
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+if(empty($name)||empty($visitor_email))
+{
+    echo"Name and email missing";
+    exit;
+}
+$email_from = 'tmauditors2@gmail.com';
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n".
+"Here is the message:\n $message".
+$to= "tmauditors2@gmail.com";
+$headers="From: $email_from \r\n";
+
+mail($to, $email_subject. $email_body, $headers);
 ?>
